000efe19b2d7bc42ed0a56021ca2c055cfc91dcc
Dan-Levi-Git <Dan.md1@gmail.com>
2020-12-02 19:53:07 +0200

    new eclipse project

---
+++ src/GBC_emu.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : GBC_emu.c
+ Author      : Dan_Levi
+ Version     :
+ Copyright   : Dan_Levi
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("test"); /* prints  */
+	return EXIT_SUCCESS;
+}
